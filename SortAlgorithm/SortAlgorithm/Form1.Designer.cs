11b0c991fc6b72aa00b781d37a425c18c580ffb9
Hira844 <64594894+Hira844@users.noreply.github.com>
2020-05-06 22:24:16 +0900

    Add files via upload

---
+++ SortAlgorithm/SortAlgorithm/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SortAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(-3, -9);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(707, 403);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Regenerate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Size:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 392);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "200";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "StartSort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 435);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Quicksand Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SortAlgorithm/SortAlgorithm/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SortAlgorithm
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+
+            Thread t = new Thread(new ThreadStart(Set));
+            t.Start();
+        }
+
+        private List<int> RandomValues = new List<int>();
+
+        private void StartSort()
+        {
+            int i = 1;
+            int[] List = RandomValues.ToArray();
+
+            while (true)
+            {
+                UpdataChart(List);
+
+                bool Check = true;
+
+                while (true)
+                {
+                    if (i + 2 > RandomValues.Count)
+                    {
+                        break;
+                    }
+
+                    int c = ComparisonValue(List[i], List[i + 1]);
+
+                    int x1 = List[i];
+                    int x2 = List[i + 1];
+
+                    if (c == 1)
+                    {
+                        List[i] = x2;
+                        List[i + 1] = x1;
+                        Check = false;
+                    }
+
+                    i += 1;
+
+                }
+
+                i = RandomValues.Count - 1;
+
+                while (true)
+                {
+                    if (i - 2 < 0)
+                    {
+                        break;
+                    }
+
+                    System.Diagnostics.Debug.WriteLine(i);
+                    int c = ComparisonValue(List[i], List[i - 1]);
+
+                    int x1 = List[i];
+                    int x2 = List[i - 1];
+
+                    if (c == 2)
+                    {
+                        List[i] = x2;
+                        List[i - 1] = x1;
+                        Check = false;
+                    }
+
+                    i -= 1;
+                }
+
+                i = 0;
+                if(Check)
+                {
+                    UpdataChart(List);
+                    break;
+                }
+            }
+
+        }
+
+        private void UpdataChart(int[] list)  
+        {
+            Invoke((MethodInvoker)(() => chart1.Series.Clear()));
+            Invoke((MethodInvoker)(() => chart1.ChartAreas.Clear()));
+
+            string chart_area1 = "Area1";
+            Invoke((MethodInvoker)(() => chart1.ChartAreas.Add(new ChartArea(chart_area1))));
+            string legend1 = "Graph1";
+            Invoke((MethodInvoker)(() => chart1.Series.Add(legend1)));
+            Invoke((MethodInvoker)(() => chart1.Series[legend1].ChartType = SeriesChartType.Column));
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Invoke((MethodInvoker)(() => chart1.Series[legend1].Points.AddY(list[i])));
+            }
+        }
+
+        private int ComparisonValue(int x1,int x2)
+        {
+            System.Diagnostics.Debug.WriteLine("X1:" + x1.ToString() + " X2:" + x2.ToString());
+            int x = 0;
+
+            if(x1 > x2)
+            {
+                x = 1;
+            }
+            else if(x1 < x2)
+            {
+                x = 2;
+            }
+            else
+            {
+                x = 0;
+            }
+
+            return x;
+        }
+
+        private Random rnd = new Random();
+
+        private void Set()
+        {
+            try
+            {
+                RandomValues = new List<int>();
+
+                Invoke((MethodInvoker)(() => chart1.Series.Clear()));
+                Invoke((MethodInvoker)(() => chart1.ChartAreas.Clear()));
+
+                string chart_area1 = "Area1";
+                Invoke((MethodInvoker)(() => chart1.ChartAreas.Add(new ChartArea(chart_area1))));
+                string legend1 = "Graph1";
+                Invoke((MethodInvoker)(() => chart1.Series.Add(legend1)));
+                Invoke((MethodInvoker)(() => chart1.Series[legend1].ChartType = SeriesChartType.Column));
+
+                for (int i = 0; i < Size; i++)
+                {
+                    int x = rnd.Next(1, 2000);
+                    Invoke((MethodInvoker)(() => chart1.Series[legend1].Points.AddY(x)));
+                    RandomValues.Add(x);
+                }
+                Invoke((MethodInvoker)(() => button1.Enabled = true));
+            }
+            catch { Set(); };
+
+        }
+        private int Size = 200;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            try
+            {
+                Size = Convert.ToInt32(textBox1.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"エラー",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            Thread t = new Thread(new ThreadStart(Set));
+            t.Start();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+           Thread t = new Thread(new ThreadStart(StartSort));
+            t.Start();
+        }
+    }
+}
